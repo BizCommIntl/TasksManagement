1d98d632404b2450417918900aa15ba7107ded89
BizCommmIntl <BizCommIntl381@gmail.com>
2023-11-05 23:29:11 +0500

    first commit

--- index.js
+++ index.js
@@ -2,19 +2,25 @@
 const express = require('express')
 
 const app = express()
-const PORT = 4000
+const PORT = 3000
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
 })
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
+// app.get('/', (req, res) => {
+//   res.send('Hey this is my API running 🥳')
+// })
+
+// app.get('/about', (req, res) => {
+//   res.send('This is my about route..... ')
+// })
+
+app.get("/", async (req, res) => {
+  //console.log('req.session.userDetail: \n',req.session.userDetail)
+      res.render("home.hbs")
 })
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
 
 // Export the Express API
 module.exports = app
